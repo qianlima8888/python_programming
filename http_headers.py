5a9099d8dc915236732f292a7c38cc8071fffb82
qianlima8888 <1058088476@qq.com>
2018-08-07 11:00:10 +0800

    http_headers
    http协议中的headers，记录方便编写爬虫程序

---
+++ http_headers.py
@@ -0,0 +1,10 @@
+headers = {
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36',
+#    'Cookie': Cookie,
+    'Connection': 'keep-alive',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Host': 'query.sse.com.cn',
+    'Referer': 'http://www.sse.com.cn/assortment/stock/list/share/'
+}
