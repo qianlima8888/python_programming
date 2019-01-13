68578a7d7f7ec2896de9f5c186c1d7f38de63915
qianlima8888 <1058088476@qq.com>
2019-01-13 09:10:33 +0800

    完善细节

--- wp_spider.py
+++ wp_spider.py
@@ -13,7 +13,7 @@ class weipu_spider:
             'Accept-Encoding'  :    'gzip, deflate',
             'Accept-Language'  :    'zh-CN,zh;q=0.8,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6',
             'Host'             :    'qikan.cqvip.com',
-            'Referer'          : 'http://www.lib.wust.edu.cn/resource/Purchased.aspx?name=&loginsubmit.x=43&loginsubmit.y=13'
+            'Referer'          :    'http://www.lib.wust.edu.cn/resource/Purchased.aspx?name=&loginsubmit.x=43&loginsubmit.y=13'
         }
 
         self.cookie = {
@@ -43,25 +43,50 @@ class weipu_spider:
         self.GetHtml("get", self.url, "0", self.headers,  False)#获得维普网站首页
 
         searchUrl = self.url + "/Qikan/Search/Index?from=Qikan_Search_Index"
-        self.GetHtml("post", searchUrl, postData, self.headers, False)#获得搜索页面
+        self.headers.pop('Referer')
+        htmlPage = self.GetHtml("post", searchUrl, postData, self.headers, False)#获得搜索页面
+        self.GetSearchResult(htmlPage)
     
     def GetHtml(self, httpMethod, targetUrl, postData, header, isPdf):#获得页面
         request = requests.Session()
 
         if httpMethod == "get":
-            page = request.get(targetUrl, headers = header)
+            page = request.get(targetUrl, headers = header, cookies = self.cookie)
         else:
             page = request.post(targetUrl, data = postData, headers = header, cookies = self.cookie)
         
-        page.encoding = "utf8"
-        print(page.text)
         return page
     
-    def GetSearchResult(self, htmlPage, pageNum):#获得搜索结果 利用beautifulSoup解析页面 获得文献标题 和下载所需的相关参数
-        htmlPage.encoding = "utf8"
+    def GetSearchResult(self, htmlPage):#获得搜索结果 利用beautifulSoup解析页面 获得文献标题 和下载所需的相关参数
+        htmlPage.encoding = "utf8"#为正常显示汉字 将编码改为utf8
         soup = BeautifulSoup(htmlPage.text,"html5lib")
-        title = soup.find_all("td", class_ = "title")
-        pass
+        #print(htmlPage.text)
+
+        #获得当前页面、页面总数和文章总数
+        current_page = soup.find("span", class_="layui-laypage-curr")
+        total_page = soup.find("a", class_="layui-laypage-last")
+        total_artical = soup.find("div", class_="layui-col-xs6 search-result")
+        print(total_artical.get_text())#输出查询到多少文献
+        print("当前为第" + current_page.get_text() + "页")
+        print("总页面数为" + total_page.get_text() + "页")
+
+        dl = soup.find_all("dl", class_="")
+
+        num = 1
+        articalDownloadInfo = {}
+        for detail in dl:
+            title =  detail.find("dt").find("a", target="_blank")
+            abstract =  detail.find("span", class_="abstract").find_all("span")[-1]
+            article_source =  detail.find("a", href="javascript:void(0);")
+            try:
+                print("----------------" + str(num) + "-----------------------", end='\n\n')
+                print("  title: " + title.get_text())               #获取文献标题
+                print("abstrcat:" + abstract.get_text())            #获得文章摘要
+                articalDownloadInfo.append(article_source["onclick"][9:-1])              #获取下载相关参数
+            except:
+                print("           该文献不能下载！")
+            print("")
+            num = num + 1
 
     def DownloadPdf(self):#下载文献
         pass
